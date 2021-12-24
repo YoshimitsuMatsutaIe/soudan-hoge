6c33d0845dec3e78a4aea31d1d8f1581adf9a12a
yoshimitsumatsutaIe <sbb03104@edu.osakafu-u.ac.jp>
2021-12-24 18:08:12 +0900

    thoth

--- o/Dynamic_Control_of_Soft_Robotic_Arm_saigen/example.py
+++ o/Dynamic_Control_of_Soft_Robotic_Arm_saigen/example.py
@@ -7,7 +7,7 @@ import numpy as np
 
 
 from simulator import Simulator
-
+from inverse_kinematics import InverseKinematics
 
 
 def pd(t):
@@ -38,19 +38,28 @@ def pd_dot_dot(t):
 
 
 
-hoge = Simulator(
-    TIME_SPAN = 2,
-    TIME_INTERVAL = 0.01,
-    pd = pd,
-    pd_dot = pd_dot,
-    pd_dot_dot = pd_dot_dot
-)
+# hoge = Simulator(
+#     TIME_SPAN = 2,
+#     TIME_INTERVAL = 0.01,
+#     pd = pd,
+#     pd_dot = pd_dot,
+#     pd_dot_dot = pd_dot_dot
+# )
 
-hoge.run_simulation()
-#hoge.plot_actuator_data()
-hoge.plot_all()
-#hoge.make_animation()
+# hoge.run_simulation()
+# #hoge.plot_actuator_data()
+# hoge.plot_all()
+# #hoge.make_animation()
 
 
 # hoge2 = Simulator()
 # hoge2.plot_test(np.array([[0.01, 0, 0]]).T)
+
+
+
+wow = InverseKinematics()
+
+a, b, c = wow.calc_desired_actuator_state_all(
+    pd, pd_dot, pd_dot_dot, 1, 0.01
+)
+print(a)
