dfdc0f888e5f6cdc820cf7b0d6a84cc2559f5b87
yoshimitsumatsutaie <matsuta2718@gmail.com>
2022-01-07 17:17:30 +0000

    hulk

--- o/soft_robot/derived/ikko_dake/eqs/julia_style/K.jl
+++ o/soft_robot/derived/ikko_dake/eqs/julia_style/K.jl
@@ -1,5 +1,12 @@
 module G
-function f(q::Vector{T}, q_dot::Vector{T}, xi::Vector{T}) where T
+function f(q::Vector{T}, q_dot::Vector{T}, xi::T) where T
+    l1 = q[1]
+    l2 = q[2]
+    l3 = q[3]
+    l1_dot = q_dot[1]
+    l2_dot = q_dot[2]
+    l3_dot = q_dot[3]
+    
     [1700    0    0;
 0 1700    0;
 0    0 1700]
