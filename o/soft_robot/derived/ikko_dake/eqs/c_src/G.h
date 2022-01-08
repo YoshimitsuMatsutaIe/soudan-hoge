eeb43b3dc38b2f2e39b23b832d8ebf48147dc7e1
yoshimitsumatsutaie <matsuta2718@gmail.com>
2022-01-08 22:23:34 +0000

    hulk

---
+++ o/soft_robot/derived/ikko_dake/eqs/c_src/G.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *                       Code generated with sympy 1.9                        *
+ *                                                                            *
+ *              See http://www.sympy.org/ for more information.               *
+ *                                                                            *
+ *                      This file is part of 'Gproject'                       *
+ ******************************************************************************/
+
+
+#ifndef GPROJECT__G__H
+#define GPROJECT__G__H
+
+void G(double l1, double l2, double l3, double *out_2812751002537589719);
+
+#endif
+
---
+++ o/soft_robot/derived/ikko_dake/eqs/c_src/Theta0.h
@@ -0,0 +1,16 @@
+/******************************************************************************
+ *                       Code generated with sympy 1.9                        *
+ *                                                                            *
+ *              See http://www.sympy.org/ for more information.               *
+ *                                                                            *
+ *                    This file is part of 'Theta0project'                    *
+ ******************************************************************************/
+
+
+#ifndef THETA0PROJECT__THETA0__H
+#define THETA0PROJECT__THETA0__H
+
+void Theta0(double l1, double l2, double l3, double xi, double *out_491730258619877890);
+
+#endif
+
