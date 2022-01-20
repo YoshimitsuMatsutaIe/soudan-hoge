6b429b6f4eb9dd489fa7b5975881a946550f0cc3
yoshimitsumatsutaie <matsuta2718@gmail.com>
2022-01-20 15:36:36 +0900

    thanos

---
+++ o/soft_robot/derivation_of_dynamics/maclaurin_2/P_0.py
@@ -0,0 +1,5 @@
+import numpy
+def f(q, xi):
+    l1, l2, l3 = q[0,0], q[1,0], q[2,0]
+
+    return (1 - numpy.cos(0.00833333333333333*xi*numpy.sqrt(l1**2 - l1*l2 - l1*l3 + l2**2 - l2*l3 + l3**2)))*(0.00625*l1 + 0.00625*l2 + 0.00625*l3 + 0.0028125)*numpy.sqrt(l1**2 - l1*l2 - l1*l3 + l2**2 - l2*l3 + l3**2)/numpy.sqrt(3*(-l2 + l3)**2/(-2*l1 + l2 + l3)**2 + 1)
---
+++ o/soft_robot/derivation_of_dynamics/maclaurin_2/R_0_2.py
@@ -0,0 +1,5 @@
+import numpy
+def f(q, xi):
+    l1, l2, l3 = q[0,0], q[1,0], q[2,0]
+
+    return numpy.sin(0.00833333333333333*xi*numpy.sqrt(l1**2 - l1*l2 - l1*l3 + l2**2 - l2*l3 + l3**2))/numpy.sqrt(3*(-l2 + l3)**2/(-2*l1 + l2 + l3)**2 + 1)
