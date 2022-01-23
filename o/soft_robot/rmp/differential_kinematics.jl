57d73b556591a3767a4ade010dd5dba7e6e71424
yoshimitsumatsutaIe <matsuta2718@gmail.com>
2022-01-23 20:24:28 +0900

    ie

--- o/soft_robot/rmp/differential_kinematics.jl
+++ o/soft_robot/rmp/differential_kinematics.jl
@@ -8,82 +8,95 @@ export J_0
 export J_1
 export J_2
 export J_3
-#export J_4
+export J_4
+
+const N = 5
 
 
-
-
-function J_0!(q::Vector{Float64}, ξ::Vector{Float64}, out::Matrix{Float64})
+function J_0!(q::Vector{Float64}, ξ::Float64, out::Matrix{Float64})
     ccall(
         (:J_0, "o/soft_robot/derivation_of_kinematics/derived/so/J_0.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """ヤコビ行列"""
-function J_0(q::Vector{Float64}, ξ::Float64, N::Int64)
-    Z = Matrix{Float64}(undef, 3, N)
-    J_0!(q, [ξ, 0, 0], Z)
-    return Z
+function J_0(q::Vector{Float64}, ξ::Float64, n::Int64)
+    Z = Matrix{Float64}(undef, 3*N, 3)
+    J_0!(q, ξ, Z)
+    return Z'[:, 1:3*n]
 end
 
 
-function J_1!(q::Vector{Float64}, ξ::Vector{Float64}, out::Matrix{Float64})
+function J_1!(q::Vector{Float64}, ξ::Float64, out::Matrix{Float64})
     ccall(
         (:J_1, "o/soft_robot/derivation_of_kinematics/derived/so/J_1.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """ヤコビ行列"""
-function J_1(q::Vector{Float64}, ξ)
-    Z = Matrix{Float64}(undef, 3, 9)
-    J_1!(q, [0, ξ, 0], Z)
-    return Z
+function J_1(q::Vector{Float64}, ξ::Float64, n::Int64)
+    Z = Matrix{Float64}(undef, 3*N, 3)
+    J_1!(q, ξ, Z)
+    return Z'[:, 1:3*n]
 end
 
 
-function J_2!(q::Vector{Float64}, ξ::Vector{Float64}, out::Matrix{Float64})
+function J_2!(q::Vector{Float64}, ξ::Float64, out::Matrix{Float64})
     ccall(
         (:J_2, "o/soft_robot/derivation_of_kinematics/derived/so/J_2.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """ヤコビ行列"""
-function J_2(q::Vector{Float64}, ξ)
-    Z = Matrix{Float64}(undef, 3, 9)
-    J_2!(q, [0, 0, ξ], Z)
-    return Z
+function J_2(q::Vector{Float64}, ξ::Float64, n::Int64)
+    Z = Matrix{Float64}(undef, 3*N, 3)
+    J_2!(q, ξ, Z)
+    return Z'[:, 1:3*n]
 end
 
 
 
-function J_3!(q::Vector{Float64}, ξ::Vector{Float64}, out::Matrix{Float64})
+function J_3!(q::Vector{Float64}, ξ::Float64, out::Matrix{Float64})
     ccall(
-        (:J_2, "o/soft_robot/derivation_of_kinematics/derived/so/J_3.so"),
+        (:J_3, "o/soft_robot/derivation_of_kinematics/derived/so/J_3.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """ヤコビ行列"""
-function J_3(q::Vector{Float64}, ξ)
-    Z = Matrix{Float64}(undef, 3, 9)
-    J_2!(q, [0, 0, ξ], Z)
-    return Z
+function J_3(q::Vector{Float64}, ξ::Float64, n::Int64)
+    Z = Matrix{Float64}(undef, 3*N, 3)
+    J_3!(q, ξ, Z)
+    return Z'[:, 1:3*n]
 end
 
 
+function J_4!(q::Vector{Float64}, ξ::Float64, out::Matrix{Float64})
+    ccall(
+        (:J_4, "o/soft_robot/derivation_of_kinematics/derived/so/J_4.so"),
+        Cvoid,
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
+        q, ξ, out
+    )
+end
 
-
+"""ヤコビ行列"""
+function J_4(q::Vector{Float64}, ξ::Float64, n::Int64)
+    Z = Matrix{Float64}(undef, 3*N, 3)
+    J_4!(q, ξ, Z)
+    return Z'[:, 1:3*n]
+end
 
 
 
@@ -109,5 +122,7 @@ q = [
     0.1, 0.2, 0.1
 ]
 
+q0 = [0.01, 0.05, 0.1]
 
-J = J_2(q, 1.0)
+J = J_2(q, 1.0, 3)
+J0 = J_0(q0, 1.0, 1)
--- o/soft_robot/rmp/kinematics.jl
+++ o/soft_robot/rmp/kinematics.jl
@@ -6,106 +6,139 @@ module Kinematics
 export Phi_0
 export Phi_1
 export Phi_2
+export Phi_3
+export Phi_4
 
 export Arm
-ctrlab2021_soudan\o\soft_robot\derivation_of_kinematics\derived\N_is_3\c_src\J_s\J_0.so
 
-function Phi_0!(q::Vector{Float64}, ξ::Vector{Float64}, out::Vector{Float64})
+
+function Phi_0!(q::Vector{Float64}, ξ::Float64, out::Vector{Float64})
     ccall(
-        (:Phi_0, "/o/soft_robot/derivation_of_kinematics/derived/N_is_3/c_src/Phi_s/Phi_0.so"),
+        (:Phi_0, "o/soft_robot/derivation_of_kinematics/derived/so/Phi_0.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """グローバル位置"""
-function Phi_0(q::Vector{Float64}, ξ)
+function Phi_0(q::Vector{Float64}, ξ::Float64)
     Z = Vector{Float64}(undef, 3)
-    Phi_0!(q, [ξ, 0, 0], Z)
+    Phi_0!(q, ξ, Z)
     return Z
 end
 
-function Phi_1!(q::Vector{Float64}, ξ::Vector{Float64}, out::Vector{Float64})
+function Phi_1!(q::Vector{Float64}, ξ::Float64, out::Vector{Float64})
     ccall(
-        (:Phi_1, "/o/soft_robot/derivation_of_kinematics/derived/N_is_3/c_src/Phi_s/Phi_1.so"),
+        (:Phi_1, "o/soft_robot/derivation_of_kinematics/derived/so/Phi_1.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """グローバル位置"""
-function Phi_1(q::Vector{Float64}, ξ)
+function Phi_1(q::Vector{Float64}, ξ::Float64)
     Z = Vector{Float64}(undef, 3)
-    Phi_1!(q, [0, ξ, 0], Z)
+    Phi_1!(q, ξ, Z)
     return Z
 end
 
-function Phi_2!(q::Vector{Float64}, ξ::Vector{Float64}, out::Vector{Float64})
+function Phi_2!(q::Vector{Float64}, ξ::Float64, out::Vector{Float64})
     ccall(
-        (:Phi_2, "/o/soft_robot/derivation_of_kinematics/derived/N_is_3/c_src/Phi_s/Phi_2.so"),
+        (:Phi_2, "o/soft_robot/derivation_of_kinematics/derived/so/Phi_2.so"),
         Cvoid,
-        (Ptr{Cdouble}, Ptr{Cdouble}, Ptr{Cdouble}),
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
         q, ξ, out
     )
 end
 
 """グローバル位置"""
-function Phi_2(q::Vector{Float64}, ξ)
+function Phi_2(q::Vector{Float64}, ξ::Float64)
     Z = Vector{Float64}(undef, 3)
-    Phi_2!(q, [0, 0, ξ], Z)
+    Phi_2!(q, ξ, Z)
     return Z
 end
 
 
+function Phi_3!(q::Vector{Float64}, ξ::Float64, out::Vector{Float64})
+    ccall(
+        (:Phi_3, "o/soft_robot/derivation_of_kinematics/derived/so/Phi_3.so"),
+        Cvoid,
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
+        q, ξ, out
+    )
+end
+
+"""グローバル位置"""
+function Phi_3(q::Vector{Float64}, ξ::Float64)
+    Z = Vector{Float64}(undef, 3)
+    Phi_3!(q, ξ, Z)
+    return Z
+end
+
+function Phi_4!(q::Vector{Float64}, ξ::Float64, out::Vector{Float64})
+    ccall(
+        (:Phi_4, "o/soft_robot/derivation_of_kinematics/derived/so/Phi_4.so"),
+        Cvoid,
+        (Ptr{Cdouble}, Cdouble, Ptr{Cdouble}),
+        q, ξ, out
+    )
+end
+
+"""グローバル位置"""
+function Phi_4(q::Vector{Float64}, ξ::Float64)
+    Z = Vector{Float64}(undef, 3)
+    Phi_4!(q, ξ, Z)
+    return Z
+end
 
 
 
 const Ξ = Vector(0:0.01:1)
 
 
-"""アームのディスク位置を計算"""
-function Arm(q::Vector{T},) where T
-    n = length(Ξ)
-    Phis = Vector{Vector{T}}(undef, 3*n)
-    for (i, ξ) in enumerate(Ξ)
-        Phis[i] = Phi_0(q, ξ)
-        Phis[i+n] = Phi_1(q, ξ)
-        Phis[i+2n] = Phi_2(q, ξ)
-        #println(Phis[i], Phis[i+n], Phis[i+2n])
-    end
-    return Phis
+# """アームのディスク位置を計算"""
+# function Arm(q::Vector{T}, N::Int64) where T
+#     n = length(Ξ)
+#     Phis = Vector{Vector{T}}(undef, N*n)
+#     for (i, ξ) in enumerate(Ξ)
+#         Phis[i] = Phi_0(q, ξ)
+#         Phis[i+n] = Phi_1(q, ξ)
+#         Phis[i+2n] = Phi_2(q, ξ)
+#         #println(Phis[i], Phis[i+n], Phis[i+2n])
+#     end
+#     return Phis
+# end
+
+# """アームのディスク位置を計算"""
+# function Arm(q::Vector{T}, m::Int64) where T
+#     n = length(Ξ)
+#     Phis = Vector{Vector{T}}(undef, n)
+#     for (i, ξ) in enumerate(Ξ)
+#         if m == 0
+#             Phis[i] = Phi_0(q, ξ)
+#         elseif m == 1
+#             Phis[i] = Phi_1(q, ξ)
+#         else
+#             Phis[i] = Phi_2(q, ξ)
+#         end
+
+
+#     end
+#     return Phis
+# end
+
 end
 
-"""アームのディスク位置を計算"""
-function Arm(q::Vector{T}, m::Int64) where T
-    n = length(Ξ)
-    Phis = Vector{Vector{T}}(undef, n)
-    for (i, ξ) in enumerate(Ξ)
-        if m == 0
-            Phis[i] = Phi_0(q, ξ)
-        elseif m == 1
-            Phis[i] = Phi_1(q, ξ)
-        else
-            Phis[i] = Phi_2(q, ξ)
-        end
+using .Kinematics
 
+q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+p0 = Phi_0(q, 0.5)
+p1 = Phi_1(q, 0.5)
+p2 = Phi_2(q, 0.5)
 
-    end
-    return Phis
-end
-
-end
-
-# using .Kinematics
-
-# q = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-# p0 = Phi_0(q, 0.5)
-# p1 = Phi_1(q, 0.5)
-# p2 = Phi_2(q, 0.5)
-
-# Arm(q, 0)
-# println(p0)
-# println(p1)
-# println(p2)
+#Arm(q, 0)
+println(p0)
+println(p1)
+println(p2)
